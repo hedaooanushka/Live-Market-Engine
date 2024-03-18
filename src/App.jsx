24b8d681c6934c2bcc202d4ebdaccc5b17a177ab
Anushka Hedaoo <73007563+hedaooanushka@users.noreply.github.com>
2024-03-19 00:49:11 +0530

    fix: footer

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Headers from './Components/Headers.jsx'
 import Search from './Components/Search.jsx'  
 import CompanyInfo from './Components/CompanyInfo.jsx'
 import Tabs from './Components/Tabs.jsx'
+import Footer from './Components/Footers.jsx'
 
 
 
@@ -14,7 +15,7 @@ function App() {
       <Headers/>
       <br/>
       <Search/>
-      <p style={{textAlign: 'center', backgroundColor: '#eee', marginBottom:'0px', height: '50px'}}><b style={{position:'relative', top:'12px'}}>Powered by: <a href="https://finnhub.io">FinnHub.io</a></b></p>
+      <Footer/>
     </>
   )
 }
