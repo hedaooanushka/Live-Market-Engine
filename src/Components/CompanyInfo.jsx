6d10883edf8971036c2ab13f1a7025686df03ca5
Anushka Hedaoo <73007563+hedaooanushka@users.noreply.github.com>
2024-03-22 01:21:08 +0530

    feat: added pipeline code for adding items to wishlist

--- src/Components/CompanyInfo.jsx
+++ src/Components/CompanyInfo.jsx
@@ -4,11 +4,11 @@ import { useState, useEffect } from 'react';
 
 
 export default function CompanyInfo(props) {
-    console.log("isdatavalid = " + props.dataValid)
-    console.log("isClick = " + props.click)
-    console.log("ticker name = " + props?.ticker_name)
-    console.log("info", props?.info)
-    console.log(props?.info?.marketStatus?.exchanges?.nasdaq)
+    // console.log("isdatavalid = " + props.dataValid)
+    // console.log("isClick = " + props.click)
+    // console.log("ticker name = " + props?.ticker_name)
+    // console.log("info", props?.info)
+    // console.log(props?.info?.marketStatus?.exchanges?.nasdaq)
     // console.log("isLoading = " + props?.isLoading)
     // const [doneLoading, setDoneLoading] = useState(false);
 
@@ -66,7 +66,14 @@ export default function CompanyInfo(props) {
             ('0' + date.getMinutes()).slice(-2) + ':' +
             ('0' + date.getSeconds()).slice(-2);
         // console.log(formattedDate);
+        const [isStarSelected, setIsStarSelected] = useState(false);
+        const [showAlert, setShowAlert] = useState(false);
         const [color, setColor] = useState('red');
+        const handleStarClick = async () => {
+            setIsStarSelected(!isStarSelected);
+            // call the backend to add the ticker to the watchlist
+            setShowAlert(true);
+        };
         const d = props?.info?.latest_price?.d;
 
         // Calculate today's date
@@ -88,11 +95,17 @@ export default function CompanyInfo(props) {
 
 
         return (
-            <>
+            <><div className='d-flex justify-content-center align-items-center'>
+                {showAlert && (
+                    <div className='alert alert-success' role='alert' style={{ textAlign: 'center', width: '60%' }}>
+                        Ticker added to watchlist
+                    </div>
+                )}
+            </div>
                 <div className="d-flex flex-row bd-highlight mx-auto" style={{ textAlign: 'center', justifyContent: 'space-around', width: '80%' }}>
                     <div className="p-2 bd-highlight">
                         <p><span style={{ fontSize: '32px', fontWeight: 'bold' }}>{props?.info?.profile?.ticker}</span>
-                            <svg className="ms-2 mb-4 bi bi-star m-1 star" type="button" xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" viewBox="0 0 16 16">
+                            <svg className="ms-2 mb-4 bi bi-star m-1 star" onClick={handleStarClick} type="button" xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill={isStarSelected ? "yellow" : "currentColor"} viewBox="0 0 16 16">
                                 <path d="M2.866 14.85c-.078.444.36.791.746.593l4.39-2.256 4.389 2.256c.386.198.824-.149.746-.592l-.83-4.73 3.522-3.356c.33-.314.16-.888-.282-.95l-4.898-.696L8.465.792a.513.513 0 0 0-.927 0L5.354 5.12l-4.898.696c-.441.062-.612.636-.283.95l3.523 3.356-.83 4.73zm4.905-2.767-3.686 1.894.694-3.957a.56.56 0 0 0-.163-.505L1.71 6.745l4.052-.576a.53.53 0 0 0 .393-.288L8 2.223l1.847 3.658a.53.53 0 0 0 .393.288l4.052.575-2.906 2.77a.56.56 0 0 0-.163.506l.694 3.957-3.686-1.894a.5.5 0 0 0-.461 0z" />
                             </svg>
                             {/* <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="yellow" class="bi bi-star-fill" viewBox="0 0 16 16">
