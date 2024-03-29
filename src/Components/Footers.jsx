ca249480c94fe07d7767748c3b5d319a5e99dc39
Prathamesh Kulkarni <prathamesh21kulkarni@gmail.com>
2024-03-29 11:13:31 +0530

    fix: removed duplicate files

--- src/Components/Footers.jsx
+++
@@ -1,8 +0,0 @@
-
-export default function Footer() {
-    return (
-        <p style={{ textAlign: 'center', backgroundColor: '#eee', marginBottom: '0px', height: '50px', position: 'fixed', bottom: '0', width: '100%', marginTop:'200px' }}>
-            <b style={{ position: 'relative', top: '12px' }}>Powered by: <a href="https://finnhub.io">FinnHub.io</a></b>
-        </p>
-    )
-}
